f9553e14296fa35c95ce7ac3fe64b21deea6ebdf
erickd3v <moyae76@gmail.com>
2024-07-12 01:53:21 -0500

    a

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+// Primero, ejecuta el comando `pnpm build` en la línea de comandos para construir el proyecto Astro
+// Esto generará el directorio `dist` con los archivos necesarios para el Server-Side Rendering (SSR)
+// Luego, importa el manejador SSR desde el archivo generado en `dist/server/entry.mjs`
+import { handler as ssrHandler } from "../dist/server/entry.mjs";
+
+const app = express();
+
+const port = process.env.PORT || 4321;
+
+const base = "/";
+
+app.use(base, express.static("dist/client"));
+app.use(ssrHandler);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
